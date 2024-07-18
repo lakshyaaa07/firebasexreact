916a98052379409ec15d4e923cd67ed62297c518
Lakshya Jain <jainlakshya07@gmail.com>
2024-07-18 23:20:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-red-500 text-xl'>Hello Coders</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkm6TB9beWl2k2N62R4xQ-QoXj7NKvX3E",
+    authDomain: "fir-blog-ba786.firebaseapp.com",
+    projectId: "fir-blog-ba786",
+    storageBucket: "fir-blog-ba786.appspot.com",
+    messagingSenderId: "73860499967",
+    appId: "1:73860499967:web:8953a0820edeb380a0ffca",
+    measurementId: "G-4XE8BLCQ6B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
